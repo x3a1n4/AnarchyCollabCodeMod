b6e169b012e62b298cb8c9efa7d4542f56cac558
Microlith57 <microlith57@gmail.com>
2021-12-05 20:39:18 +1300

    Initial setup

---
+++ AnarchyCollabModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Celeste.Mod.AnarchyCollab {
+    public class AnarchyCollabModule : EverestModule {
+        
+        public static AnarchyCollabModule Instance;
+
+        public AnarchyCollabModule() {
+            Instance = this;
+        }
+
+        public override void Load() {
+        }
+
+        public override void Unload() {
+        }
+
+    }
+}
